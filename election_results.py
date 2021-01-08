1eb2aa1da395f21bb10409365280ccffaba244aa
v2krishna <v2krishna@gmail.com>
2021-01-07 22:43:37 -0600

    added new python files

---
+++ election_results.py
@@ -0,0 +1,6 @@
+my_votes = int(input("Number of votes?"))
+total_votes = int(input("Total votes registered?"))
+
+percentage_votes = (my_votes/total_votes) * 100 
+
+print(f"percent of votes : {percentage_votes} " )
