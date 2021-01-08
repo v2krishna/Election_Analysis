aaaeaa7ea517f743f41345db1ef01b1295742446
v2krishna <v2krishna@gmail.com>
2021-01-07 23:15:14 -0600

    comment

---
+++ forloop.py
@@ -0,0 +1,11 @@
+counties_tuple = ("Arapahoe","Denver","Jefferson")
+
+for i in range(len(counties_tuple)):
+      print(counties_tuple[i])
+
+for county in counties_tuple:
+      print(county)
+
+
+for i in len(counties_tuple):
+      print(counties_tuple[i])
